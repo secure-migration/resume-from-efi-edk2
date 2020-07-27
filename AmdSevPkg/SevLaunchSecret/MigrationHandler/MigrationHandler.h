407c6e789c05060dbb32b188ac008a042350d835
Dov Murik <dov.murik1@il.ibm.com>
2020-07-27 12:34:44 +0000

    Extract struct cpu_state to State.h

--- AmdSevPkg/SevLaunchSecret/MigrationHandler/MigrationHandler.h
+++ AmdSevPkg/SevLaunchSecret/MigrationHandler/MigrationHandler.h
@@ -10,55 +10,6 @@
 
 #include <string.h>
 
-/* 
- * TARGET STATE 
- *
- * For now, we store the target state in OVMF using 
- * the following structures.
- *
- * pr_regs is from Linux kernel: arch/x86/include/asm/ptrace.h
- */
-struct pt_regs {
-/*
- * C ABI says these regs are callee-preserved. They aren't saved on kernel entry
- * unless syscall needs a complete, fully filled "struct pt_regs".
- */
-	unsigned long r15;
-	unsigned long r14;
-	unsigned long r13;
-	unsigned long r12;
-	unsigned long bp;
-	unsigned long bx;
-/* These regs are callee-clobbered. Always saved on kernel entry. */
-	unsigned long r11;
-	unsigned long r10;
-	unsigned long r9;
-	unsigned long r8;
-	unsigned long ax;
-	unsigned long cx;
-	unsigned long dx;
-	unsigned long si;
-	unsigned long di;
-/*
- * On syscall entry, this is syscall#. On CPU exception, this is error code.
- * On hw interrupt, it's IRQ number:
- */
-	unsigned long orig_ax;
-/* Return frame for iretq */
-	unsigned long ip;
-	unsigned long cs;
-	unsigned long flags;
-	unsigned long sp;
-	unsigned long ss;
-/* top of stack page */
-};
-
-// This is from Linux kernel: arch/x86/include/asm/desc_defs.h
-struct desc_ptr {
-	unsigned short size;
-	unsigned long address;
-} __attribute__((packed)) ;
-
 //
 // Variables used by RestoreState.nasm
 //
@@ -76,20 +27,6 @@ UINT64 gTempPGT;
 UINT64 gMMUCR4Features;
 UINT64 gRelocatedRestoreStep2;
 
-#pragma pack(1)
-struct cpu_state {
-  char magic[8];
-  UINT64 version;
-  struct pt_regs regs;
-  UINT64 rip;
-  UINT64 cr0;
-  UINT64 cr2;
-  UINT64 cr3;
-  UINT64 cr4;
-  struct desc_ptr gdt_desc;
-  char end_magic[8];
-};
-#pragma pack()
 /*
  * CREATING INTERMEDIATE PAGE TABLES
  *
---
+++ AmdSevPkg/SevLaunchSecret/MigrationHandler/State.h
@@ -0,0 +1,70 @@
+/* 
+ * TARGET STATE 
+ *
+ * For now, we store the target state in OVMF using 
+ * the following structures.
+ *
+ * pr_regs is from Linux kernel: arch/x86/include/asm/ptrace.h
+ */
+struct pt_regs {
+/*
+ * C ABI says these regs are callee-preserved. They aren't saved on kernel entry
+ * unless syscall needs a complete, fully filled "struct pt_regs".
+ */
+	unsigned long r15;
+	unsigned long r14;
+	unsigned long r13;
+	unsigned long r12;
+	unsigned long bp;
+	unsigned long bx;
+/* These regs are callee-clobbered. Always saved on kernel entry. */
+	unsigned long r11;
+	unsigned long r10;
+	unsigned long r9;
+	unsigned long r8;
+	unsigned long ax;
+	unsigned long cx;
+	unsigned long dx;
+	unsigned long si;
+	unsigned long di;
+/*
+ * On syscall entry, this is syscall#. On CPU exception, this is error code.
+ * On hw interrupt, it's IRQ number:
+ */
+	unsigned long orig_ax;
+/* Return frame for iretq */
+	unsigned long ip;
+	unsigned long cs;
+	unsigned long flags;
+	unsigned long sp;
+	unsigned long ss;
+/* top of stack page */
+};
+
+// This is from Linux kernel: arch/x86/include/asm/desc_defs.h
+struct desc_ptr {
+	unsigned short size;
+	unsigned long address;
+} __attribute__((packed)) ;
+
+#pragma pack(1)
+struct cpu_state {
+  char magic[8];
+  UINT64 version;
+  struct pt_regs regs;
+  UINT16 ds;
+  UINT16 es;
+  UINT16 fs;
+  UINT16 gs;
+  UINT64 cr0;
+  UINT64 cr2;
+  UINT64 cr3;
+  UINT64 cr4;
+  UINT64 efer;
+  UINT16 gdt_pad;
+  struct desc_ptr gdt_desc;
+  UINT16 idt_pad;
+  struct desc_ptr idt;
+  char end_magic[8];
+};
+#pragma pack()
