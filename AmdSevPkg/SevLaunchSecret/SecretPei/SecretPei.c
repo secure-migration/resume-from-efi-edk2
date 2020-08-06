bfe2b48c580087356a57d087f3199734d786ab3b
Dov Murik <dov.murik1@il.ibm.com>
2020-08-06 06:31:58 +0000

    MigrationHandler: SecretPei: mark migration pages as BootServices

--- AmdSevPkg/SevLaunchSecret/SecretPei/SecretPei.c
+++ AmdSevPkg/SevLaunchSecret/SecretPei/SecretPei.c
@@ -21,10 +21,21 @@ InitializeSecretPei (
     PcdGet32 (PcdSevMigrationMailboxSize),
     EfiRuntimeServicesData);
 
+  //
+  // This region supposedly should be of type EfiRuntimeServicesData, because
+  // we want the kernel not to use this physical memory area. However, if it is
+  // marked as Runtime, then the kernel doesn't include this are in its kernel
+  // mapping (page offset), and this makes it harder to jump to our page with a
+  // kernel-valid virtual address.
+  //
+  // For now we mark this as Boot services, which makes the kernel map these
+  // pages at virtual address (_PAGE_OFFSET+phys_addr); hopefully the kernel
+  // doesn't use these 12K for something important.
+  //
   BuildMemoryAllocationHob (
     PcdGet32 (PcdSevMigrationPagesBase),
     PcdGet32 (PcdSevMigrationPagesSize),
-    EfiRuntimeServicesData);
+    EfiBootServicesData /* <--- this is the hack */);
 
   BuildMemoryAllocationHob (
     PcdGet32 (PcdSevMigrationStatePageBase),
