e9b2281edb58913b208c21d6dc42a4c68e50c3b1
Tobin Feldman-Fitzthum <tobin@ibm.com>
2020-09-28 22:07:38 +0000

    clean up migration handler

--- AmdSevPkg/SevLaunchSecret/MigrationHandler/MigrationHandler.c
+++ AmdSevPkg/SevLaunchSecret/MigrationHandler/MigrationHandler.c
@@ -1,6 +1,8 @@
 /*
  *  Migration Handler
  *
+ *  UEFI Application that resumes into a VM Snapshot
+ *
  */
 #include "State.h"
 #include "StateLayout.h"
@@ -12,7 +14,7 @@ void RestoreRegisters(void);
 void RestoreStep1(void);
 void RestoreStep2(void);
 
-// helpers for pagetable walk
+// helpers for pagetables. borrowed from Linux
 
 static inline pudval_t native_pud_val(pud_t pud)
 {
@@ -42,7 +44,6 @@ static inline pudval_t pud_flags(pud_t pud)
         return native_pud_val(pud) & pud_flags_mask(pud);
 }
 
-#define mk32 (((UINT64)1 << 32) - 1)
 static inline pmdval_t pmd_pfn_mask(pmd_t pmd)
 {
 	if (native_pmd_val(pmd) & _PAGE_PSE)
@@ -63,7 +64,6 @@ static inline pmdval_t pmd_flags(pmd_t pmd)
 
 static inline unsigned long pgd_page_vaddr(pgd_t pgd)
 {
-	//return (unsigned long)__va((unsigned long)pgd_val(pgd) & PTE_PFN_MASK & mk32);
     return pgd_val(pgd) & PTE_PFN_MASK;
 }
 
@@ -94,7 +94,8 @@ static inline pte_t *pte_offset_kernel(pmd_t *pmd, unsigned long address)
 	return (pte_t *)pmd_page_vaddr(*pmd) + pte_index(address);
 }
 
-
+// a function for testing page tables. this will traverse 
+// a given page table tree and resolve a va to a pa.
 int GetPa(UINT64 pgd_base, unsigned long long va){
     pgd_t *pgd;
     pud_t *pud;
@@ -136,6 +137,7 @@ int GetPa(UINT64 pgd_base, unsigned long long va){
     return 0;
 }
 
+// This is a temporary workaround. See main for details
 int ClearPageNXFlag(UINT64 pgd_base, unsigned long long va){
     pgd_t *pgd;
     pud_t *pud;
@@ -174,44 +176,25 @@ int ClearPageNXFlag(UINT64 pgd_base, unsigned long long va){
     return 0;
 }
 
-// this seems a bit too simple. for one thing, are we actually doing 
-// all four levels? 
-// do we also need to mark the page as ex?
+
+// Add a page to a page table tree. This will create a three-level
+// mapping. Mostly borrowed from Linux.
 static void AddPageToMapping(unsigned long va, unsigned long pa){
   pgd_t new_pgd;
   pud_t new_pud;
   pmd_t new_pmd;
-  /* pte_t new_pte; */
 
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER: Mapping 0x%llx to 0x%llx \n", va, pa);
   pgprot_t pgtable_prot = __pgprot(_KERNPG_TABLE);
   pgprot_t pmd_text_prot = __pgprot(__PAGE_KERNEL_LARGE_EXEC);
 
-  /* Filter out unsupported __PAGE_KERNEL* bits: */
-  // look into this more
-  // does nothing with our current setup
   pgprot_val(pmd_text_prot) &= __default_kernel_pte_mask;
   pgprot_val(pgtable_prot)  &= __default_kernel_pte_mask;
 
-  //new_pte = __pte(pa & PTE_MASK); // TODO | page-flags
-  //DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER: AddPageToMapping: va=%xllx pte_index=%x\n", va, pte_index(va));
-  //CopyMem(pte + pte_index(va),&new_pte,sizeof(pte_t));
-
-  // i think we can just use memcpy here
-  // here we are setting an entry in the pmd,
-  // the destination is an index into the pmd that corresponds 
-  // with the virtual address 
-  // the source is the entry specifying the physical addres 
   new_pmd = __pmd((pa & PMD_MASK) | pgprot_val(pmd_text_prot));
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER: AddPageToMapping: va=0x%llx pmd_index(va)=0x%x\n", va, pmd_index(va));
   CopyMem(pmd + pmd_index(va),&new_pmd,sizeof(pmd_t));
 
-  // basically the same 
-  // in the kernel, this line uses the __pa macro to find the 
-  // the phyiscal address of the omd (this entry points to the 
-  // next node in the tree). since OVMF is direct-mapped, 
-  // i think we can just use the address of the pmd directly. 
-  // have to do some suspicious casting of pmd. 
   new_pud = __pud((UINT64)pmd | pgprot_val(pgtable_prot));
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER: AddPageToMapping: va=0x%llx pud_index(va)=0x%x\n", va, pud_index(va));
   CopyMem(pud + pud_index(va),&new_pud,sizeof(pud_t));
@@ -220,7 +203,6 @@ static void AddPageToMapping(unsigned long va, unsigned long pa){
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER: AddPageToMapping: va=0x%llx pgd_index(va)=0x%x\n", va, pgd_index(va));
   CopyMem(pgd + pgd_index(va), &new_pgd, sizeof(pgd_t));
 
-  // i think we need something with the pte as well
 }
 
 // setup a page table for stage 2 of the trampoline 
@@ -254,31 +236,18 @@ MigrationHandlerMain(
   IN EFI_SYSTEM_TABLE     *SystemTable
   )
 {
-  // Setup the mailbox
-  UINT64 params_base = PcdGet32(PcdSevMigrationMailboxBase); 
-  volatile struct sev_mh_params *params = (void *) params_base;   
-
+  // The cpu state from the source is passed in via FW_CFG and 
+  // copied to this page.
   UINT64 state_page_base = PcdGet32(PcdSevMigrationStatePageBase); 
   struct cpu_state *SourceState = (void *) state_page_base;   
 
-  // Change to test TestTarget's VA:
-  //DebugPrint(DEBUG_ERROR,"MH: Changing target RIP to TestTarget (FAKESTATE)\n");
-  //SourceState->regs.ip = 0xffff88800080cd00;
-
-  // Add 16 to RIP to skip zzzloop:
-  //DebugPrint(DEBUG_ERROR,"MH: Adding 16 to target RIP to skip zzzloop\n");
-  //SourceState->regs.ip += 0x10;
-
+  // we can access parts of this page via the pt_regs struct
+  // we mainly use this for testing
+  // maybe remove some of the testing code here
   struct pt_regs source_regs = SourceState->regs;
   DebugPrint(DEBUG_ERROR,"MH: Looking for RIP in source pgt\n");
   GetPa(cr3_to_pgt_pa(SourceState->cr3), source_regs.ip);
-
-  //DebugPrint(DEBUG_ERROR,"MH: Looking for RSP in source pgt\n");
-  //GetPa(cr3_to_pgt_pa(SourceState->cr3), source_regs.sp);
-
-  // Trampoline code can live here temporarily.
   
-  // populate our state structs
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER Address of RestoreRegisters = %p\n", RestoreRegisters);
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER Address of RestoreRegistersData = %p\n", &RestoreRegistersData);
 
@@ -289,27 +258,21 @@ MigrationHandlerMain(
   gMMUCR4Features = SourceState->cr4;
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER current CR4 = 0x%lx gMMUCR4Features = 0x%lx\n", AsmReadCr4(), gMMUCR4Features);
 
-  // relocate pages
+  // We need to be somewhat careful about how we setup the 
+  // pages for our trampoline.
+  // The trampoline has three stages, which each have one page, 
+  // plus an addiditional page that stores the CPU state from 
+  // the source. 
+  //
+  // We can put the page for the first stage just about anywhere, 
+  // so we won't move it. The pages for the second two stages 
+  // need to be mapped in our intermediate page table. First, 
+  // we will move them to a known address that we set aside 
+  // with a HOB in PEI.
   gRelocatedRestoreStep2 = PcdGet32(PcdSevMigrationPagesBase);
-
-  //---------------------------
-  // This commented-out section is an attempt to copy our code+data to pages
-  // allocated by AllocatePages instead of pages reserved by Pcd.
-  //
-  // gBS is NULL, so instaed I use SystemTable->BootServices
-  //DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER Before AllocatePages\n");
-  //EFI_PHYSICAL_ADDRESS StartAddress = 0x0;
-  //EFI_STATUS Status = SystemTable->BootServices->AllocatePages (AllocateAnyPages, EfiRuntimeServicesCode, 3, &StartAddress);
-  ////EFI_STATUS Status = SystemTable->BootServices->AllocatePages (AllocateAnyPages, EfiRuntimeServicesData, 3, &StartAddress);
-  ////EFI_STATUS Status = SystemTable->BootServices->AllocatePages (AllocateAnyPages, EfiPersistentMemory, 3, &StartAddress);
-  //DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER After  AllocatePages Status = %d StartAddress = %llx\n", Status, StartAddress);
-  //ASSERT_EFI_ERROR (Status);
-  //gRelocatedRestoreStep2 = StartAddress;
-  //
-  //---------------------------
-
   gRelocatedRestoreRegisters = gRelocatedRestoreStep2 + PAGE_SIZE;
   gRelocatedRestoreRegistersData = gRelocatedRestoreRegisters + PAGE_SIZE;
+
   UINT64 gRelocatedRestoreRegistersDataStart = gRelocatedRestoreRegistersData + CPU_STATE_OFFSET_IN_PAGE; // Extra 8 bytes so the IRETQ frame is 16-bytes aligned
 
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER New pages: gRelocatedRestoreStep2 = %lx\n", gRelocatedRestoreStep2);
@@ -318,47 +281,14 @@ MigrationHandlerMain(
 
   CopyMem((void *)gRelocatedRestoreStep2,RestoreStep2,PAGE_SIZE);
   CopyMem((void *)gRelocatedRestoreRegisters,RestoreRegisters,PAGE_SIZE);
-  // i don't think we actaully need to copy this. SourceState is already 
-  // on its own page. we should be able to just add that page to the 
-  // intermediate pagetable and set gRelocatedRestoreRegistersData 
-  // accordingly. just going to leave for now
+  
   ZeroMem((void *)gRelocatedRestoreRegistersData, PAGE_SIZE);
   CopyMem((void *)gRelocatedRestoreRegistersDataStart,SourceState,sizeof(*SourceState));
 
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER New pages: content of gRelocatedRestoreRegistersDataStart = %a\n", (char*)((void*)gRelocatedRestoreRegistersDataStart));
 
+  // Now add the mappings for stages two and three. 
   GenerateIntermediatePageTables();
-
-#if 0
-  DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER pgd = \n");
-  for (int i = 0; i < ENTRIES; i++) {
-    DebugPrint(DEBUG_ERROR,"%llx ", pgd[i]);
-  }
-  DebugPrint(DEBUG_ERROR,"\n");
-  DebugPrint(DEBUG_ERROR,"pgd[0x111]=%llx\n", pgd[0x111]);
-
-  DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER pud = \n");
-  for (int i = 0; i < ENTRIES; i++) {
-    DebugPrint(DEBUG_ERROR,"%llx ", pud[i]);
-  }
-  DebugPrint(DEBUG_ERROR,"\n");
-  DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER pmd = \n");
-  for (int i = 0; i < ENTRIES; i++) {
-    DebugPrint(DEBUG_ERROR,"%llx ", pmd[i]);
-  }
-  DebugPrint(DEBUG_ERROR,"\n");
-  DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER pte = \n");
-  for (int i = 0; i < ENTRIES; i++) {
-    DebugPrint(DEBUG_ERROR,"%llx ", pte[i]);
-  }
-  DebugPrint(DEBUG_ERROR,"\n");
-  DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER target CR3 pgd = \n");
-  UINT64* target_pgd = (void*)(SourceState->cr3);
-  for (int i = 0; i < ENTRIES; i++) {
-    DebugPrint(DEBUG_ERROR,"%llx ", target_pgd[i]);
-  }
-  DebugPrint(DEBUG_ERROR,"\n");
-#endif
 
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER   Temp PGD = 0x%lx\n", cr3_to_pgt_pa(pgd));
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER Target PGD = 0x%lx\n", cr3_to_pgt_pa(SourceState->cr3));
@@ -368,126 +298,26 @@ MigrationHandlerMain(
 
   // Switch to the copy of the code in the target's address space
   gRelocatedRestoreRegisters = (unsigned long)__va(gRelocatedRestoreRegisters);
-  // Modify the target's page table to make our page executable
+  // The final page of the trampoline must be mapped in the
+  // source page table. The source table will be the kernel's
+  // offset map. Currently we just assume that a mostly arbitrary
+  // location will be mapped. For that to work, we need to modify
+  // the NX bit of the source page table. 
   ClearPageNXFlag(cr3_to_pgt_pa(SourceState->cr3), gRelocatedRestoreRegisters);
   GetPa(cr3_to_pgt_pa(pgd), gRelocatedRestoreRegisters);
   GetPa(cr3_to_pgt_pa(SourceState->cr3), gRelocatedRestoreRegisters);
 
-  //DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER GetPa of Source GDT\n");
-  //GetPa(cr3_to_pgt_pa(SourceState->cr3), 0xfffffe0000001000ULL);
-  //DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER GetPa of Source IDT\n");
-  //GetPa(cr3_to_pgt_pa(SourceState->cr3), 0xfffffe0000000000ULL);
 
-  // This can help with gdb:
-  //
-  volatile int wait = 0;
-  while (wait) {
-    __asm__ __volatile__("pause");
-  }
-
-  //UINT64 handler_addr = 0xffff88800080cd00;
-  //UINT64 dummy_idt[2];
-  //dummy_idt[0] = (handler_addr & 0xffffULL) | (0x0010ULL << 16) | (0x8e00ULL << 32) | (((handler_addr >> 16) & 0xffffULL) << 48);
-  //dummy_idt[1] = handler_addr >> 32;
-  //DebugPrint(DEBUG_ERROR,"dummy_idt[0] = %016llx\n", dummy_idt[0]);
-  //DebugPrint(DEBUG_ERROR,"dummy_idt[1] = %016llx\n", dummy_idt[1]);
-
-  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"MigrationHandler: Calling RestoreStep1\r\n");
+  //SystemTable->ConOut->OutputString(SystemTable->ConOut, L"MigrationHandler: Calling RestoreStep1\r\n");
   DebugPrint(DEBUG_ERROR,"MIGRATION HANDLER calling RestoreStep1\n");
 
-#if 0
-  // TODO this should be recorded in cpu_state and restored from there.
-  //
-  // GDT area (virtual linux addresses):
-  //
-  // fffffe0000001000: 0x0000000000000000 0x00cf9b000000ffff
-  // fffffe0000001010: 0x00af9b000000ffff 0x00cf93000000ffff
-  // fffffe0000001020: 0x00cffb000000ffff 0x00cff3000000ffff
-  // fffffe0000001030: 0x00affb000000ffff 0x0000000000000000
-  // fffffe0000001040: 0x00008b0030004087 0x00000000fffffe00
-  // fffffe0000001050: 0x0000000000000000 0x0000000000000000
-  // fffffe0000001060: 0x0000000000000000 0x0000000000000000
-  // fffffe0000001070: 0x0000000000000000 0x0040f50000000000
-  //
-  // Fix Linux GDT entries overwritten by OVMF.
-  //
-  // This might actually write over some OVMF libary code, so do this as the
-  // last thing before calling out to our assembly functions (RestoreStep1).
-  UINT64* linux_gdt = (void*)0x3f80b000; // <-- this is the physical address but we're in identity mapping in OVMF
-  linux_gdt[ 0] = 0x0000000000000000ULL;
-  linux_gdt[ 1] = 0x00cf9b000000ffffULL;
-  linux_gdt[ 2] = 0x00af9b000000ffffULL; // CS=0x0010 (SI=2 TI=0 RPL=0)
-  linux_gdt[ 3] = 0x00cf93000000ffffULL; // SS=0x0018 (SI=3 TI=0 RPL=0)
-  linux_gdt[ 4] = 0x00cffb000000ffffULL;
-  linux_gdt[ 5] = 0x00cff3000000ffffULL;
-  linux_gdt[ 6] = 0x00affb000000ffffULL;
-  linux_gdt[ 7] = 0x0000000000000000ULL;
-  linux_gdt[ 8] = 0x00008b0030004087ULL;
-  linux_gdt[ 9] = 0x00000000fffffe00ULL;
-  linux_gdt[10] = 0x0000000000000000ULL;
-  linux_gdt[11] = 0x0000000000000000ULL;
-  linux_gdt[12] = 0x0000000000000000ULL;
-  linux_gdt[13] = 0x0000000000000000ULL;
-  linux_gdt[14] = 0x0000000000000000ULL;
-  linux_gdt[15] = 0x0040f50000000000ULL;
-
-  UINT64* my_idt = (void*)0x311c000; // <-- this is the physical address but we're in identity mapping in OVMF
-  for (int i = 0; i < 256; i++) {
-    //if (i == 11 /* NP */ || i == 12 || i == 13 /* GP */ || i == 14 /* PF */ ) {
-    if (0){
-     //   (i >= 11 && i <= 17) {
-        // fffffe0000000ff0: 0x90 0x0c 0x10 0x00 0x00 0x8e 0xc0 0x81
-        //
-        // 0x81c08e0000100c90
-        //
-        // fffffe0000000ff8: 0xff 0xff 0xff 0xff 0x00 0x00 0x00 0x00
-        //
-      my_idt[i * 2] = (handler_addr & 0xffffULL) | (0x0010ULL << 16) | (0x8e00ULL << 32) | (((handler_addr >> 16) & 0xffffULL) << 48);
-      my_idt[i * 2 + 1] = handler_addr >> 32;
-
-    } else {
-      my_idt[i * 2] = 0;
-      my_idt[i * 2 + 1] = 0;
-    }
-  }
-
-  // first two entries from linux IDT
-  my_idt[0] = 0x81c08e0000100870;
-  my_idt[1] = 0x00000000ffffffff;
-  // intr 0  handler 0xffffffff81c00870
-  my_idt[2] = 0x81c08e0300100b40;
-  my_idt[3] = 0x00000000ffffffff;
-#endif
-
+  // start the trampoline
   RestoreStep1(); 
 
-  return 0;
-
-  // Eventually we will use this loop to check the mailbox 
-  // and encrypt/decrypt pages or trampoline on command.
-  while(1) {
-    
-    // wait for command
-    while(!params->go);
-    
-    // handle commands
-    switch (params->nr){
-      case 0:
-        // do something 
-        break;
-
-    }
-
-    params->go = 0;
-    params->done = 1;
-         
-  }
   return 0;
 }
 
 
-// seems to start here no matter how ENTRY_POINT 
-// is set in INF
 EFI_STATUS 
 EFIAPI
 _ModuleEntryPoint(
