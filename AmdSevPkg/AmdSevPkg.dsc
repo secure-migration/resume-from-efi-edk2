b39efdc0192bd8d5afea6e02f031f9a0e70f342c
Tobin Feldman-Fitzthum <tobin@ibm.com>
2020-10-05 22:11:47 +0000

    move PEI

--- AmdSevPkg/AmdSevPkg.dsc
+++ AmdSevPkg/AmdSevPkg.dsc
@@ -668,7 +668,7 @@
   OvmfPkg/SmmAccess/SmmAccessPei.inf
 !endif
   UefiCpuPkg/CpuMpPei/CpuMpPei.inf
-  AmdSevPkg/SevLaunchSecret/SecretPei/SecretPei.inf
+  #AmdSevPkg/SevLaunchSecret/SecretPei/SecretPei.inf
 
 !if $(TPM_ENABLE) == TRUE
   OvmfPkg/Tcg/Tcg2Config/Tcg2ConfigPei.inf
--- OvmfPkg/PlatformPei/AmdSev.c
+++ OvmfPkg/PlatformPei/AmdSev.c
@@ -36,6 +36,31 @@ AmdSevInitialize (
   UINT64                            EncryptionMask;
   RETURN_STATUS                     PcdStatus;
 
+  // For the demo, we need to setup a few HOBs even if
+  // SEV is not enabled
+  DebugPrint(DEBUG_ERROR,"ADDING DEMO HOBS\n");
+
+  // This region supposedly should be of type EfiRuntimeServicesData, because
+  // we want the kernel not to use this physical memory area. However, if it is
+  // marked as Runtime, then the kernel doesn't include this are in its kernel
+  // mapping (page offset), and this makes it harder to jump to our page with a
+  // kernel-valid virtual address.
+  //
+  // For now we mark this as Boot services, which makes the kernel map these
+  // pages at virtual address (_PAGE_OFFSET+phys_addr); hopefully the kernel
+  // doesn't use these 12K for something important.
+  //
+  BuildMemoryAllocationHob (
+    PcdGet32 (PcdSevMigrationPagesBase),
+    PcdGet32 (PcdSevMigrationPagesSize),
+    EfiBootServicesData /* <--- this is the hack */);
+
+  BuildMemoryAllocationHob (
+    PcdGet32 (PcdSevMigrationStatePageBase),
+    PcdGet32 (PcdSevMigrationStatePageSize),
+    EfiRuntimeServicesData);
+
+
   //
   // Check if SEV is enabled
   //
